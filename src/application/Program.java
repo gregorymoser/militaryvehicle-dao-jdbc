e99e35342e041fb5d1efdfca81d4563d14c9675f
Gregory Moser <gdc.moser@hotmail.com>
2020-04-03 17:51:26 +0100

    Upgrades

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,12 @@ public class Program {
 	
 	public static void main (String args[]) {
 	
-	Company company = new Company(1, "1a Cia Fuz");
-	Vehicle vehicle = new Vehicle(10, "Marrua", "1A2B3C", new Date(), 1000.0, company);
-	
-	VehicleDao vehicleDao = DaoFactory.createVehicleDao();
-	
-	System.out.println(vehicle);
-	
+		Company company = new Company(1, "1a Cia Fuz");
+		Vehicle vehicle = new Vehicle(10, "Marrua", "1A2B3C", new Date(), 1000.0, company);
+		
+		VehicleDao vehicleDao = DaoFactory.createVehicleDao();
+		
+		System.out.println(vehicle);
+
 	}
 }
